6547be7189cbd3b20eebb619bc102bc86f13f827
Cid <dev.debojyoti17@gmail.com>
2025-06-05 11:43:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.cpp
+++ Difficulty: Easy/Floor in a Sorted Array/floor-in-a-sorted-array.cpp
@@ -1,72 +1,34 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
 
     int findFloor(vector<int>& arr, int x) {
         // Your code here
-        int st = 0;
+        int st =0;
         int end = arr.size()-1;
-        int index = -1,i=-1;
+        
+        int priority1 = -1,priority2 = -1;
         
         while(st<=end){
             
-            int mid = (st+end)/2;
+            int mid = (st + (end - st)/2);
             
             if(arr[mid]==x){
-                index = mid;
+                priority1 = mid;
                 st = mid+1;
             }
-            
-            else if(arr[mid]<x){
-                i = mid;
-                st = mid+1;
-            }
-            
             else if(arr[mid]>x){
                 end = mid-1;
             }
+            else if(arr[mid]<x){
+                priority2 = mid;
+                st = mid+1;
+            }
         }
-        
-        if(index == -1 && i==-1)
+        if(priority1 ==-1 && priority2 == -1)
         return -1;
-        else if(index!=-1)
-        return index;
-        else 
-        return i;
+        else if(priority1!=-1)
+        return priority1;
+        else
+        return priority2;
     }
 };
-
-
-
-//{ Driver Code Starts.
-int main() {
-    string ts;
-    getline(cin, ts);
-    int t = stoi(ts);
-    while (t--) {
-
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        string ks;
-        getline(cin, ks);
-        int x = stoi(ks);
-        Solution ob;
-        int ans = ob.findFloor(arr, x);
-
-        cout << ans << endl;
-    }
-    return 0;
-}
-// } Driver Code Ends
