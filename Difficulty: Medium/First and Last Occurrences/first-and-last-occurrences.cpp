3e1fc960c8af83311dd03e925553eb31cd9b41d4
Cid <dev.debojyoti17@gmail.com>
2025-06-05 10:30:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.cpp
+++ Difficulty: Medium/First and Last Occurrences/first-and-last-occurrences.cpp
@@ -1,88 +1,45 @@
-//{ Driver Code Starts
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     vector<int> find(vector<int>& arr, int x) {
         // code here
-        int st = 0, end = arr.size()-1;
-        int mid;
-        int startindex = -1,endindex = -1;
+        int st = 0;
+        int end = arr.size()-1;
+        int firstoccurence = -1;
+        int lastoccurence = -1;
         
         while(st<=end){
             
-            mid = st + ((end - st)/2);
-            
-            if(arr[mid]<x){
-                st = mid+1;
+            int mid = (st + (end-st)/2);
+            if(arr[mid]==x){
+                firstoccurence = mid;
+                end = mid-1;
             }
             else if(arr[mid]>x){
                 end = mid-1;
             }
-            else if(arr[mid]==x){
-                startindex = mid;
-                end = mid-1;
+            else if(arr[mid]<x){
+                st = mid+1;
             }
         }
         
         st = 0;
-        end = arr.size()-1;
+        end= arr.size()-1;
         
         while(st<=end){
             
-            mid = st + ((end - st)/2);
-            
-            if(arr[mid]<x){
+            int mid = (st + (end-st)/2);
+            if(arr[mid]==x){
+                lastoccurence = mid;
                 st = mid+1;
             }
             else if(arr[mid]>x){
                 end = mid-1;
             }
-            else if(arr[mid]==x){
-                endindex = mid;
+            else if(arr[mid]<x){
                 st = mid+1;
             }
         }
         
-        return {startindex, endindex};
-        
-        
-        
+        return {firstoccurence,lastoccurence};
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        int x;
-        cin >> x;
-        cin.ignore();
-        vector<int> ans;
-        Solution ob;
-        ans = ob.find(arr, x);
-        cout << ans[0] << " " << ans[1] << endl;
-        cout << "~" << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
