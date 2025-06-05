b8b26fab1becd451e4fd0ac6cd7978e52c7ef863
Cid <dev.debojyoti17@gmail.com>
2025-06-05 10:35:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
@@ -1,99 +1,50 @@
-//{ Driver Code Starts
-// Initial function template for C++
-
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
   public:
     int countFreq(vector<int>& arr, int target) {
         // code here
-        int st = 0, end = arr.size()-1;
-        int mid;
-        int startindex = -1,endindex = -1;
+        int st = 0;
+        int end = arr.size()-1;
+        int firstoccurence = -1;
+        int lastoccurence = -1;
         
         while(st<=end){
             
-            mid = st + ((end - st)/2);
-            
-            if(arr[mid]<target){
-                st = mid+1;
+            int mid = (st + (end-st)/2);
+            if(arr[mid]==target){
+                firstoccurence = mid;
+                end = mid-1;
             }
             else if(arr[mid]>target){
                 end = mid-1;
             }
-            else if(arr[mid]==target){
-                startindex = mid;
-                end = mid-1;
+            else if(arr[mid]<target){
+                st = mid+1;
             }
         }
         
         st = 0;
-        end = arr.size()-1;
+        end= arr.size()-1;
         
         while(st<=end){
             
-            mid = st + ((end - st)/2);
-            
-            if(arr[mid]<target){
+            int mid = (st + (end-st)/2);
+            if(arr[mid]==target){
+                lastoccurence = mid;
                 st = mid+1;
             }
             else if(arr[mid]>target){
                 end = mid-1;
             }
-            else if(arr[mid]==target){
-                endindex = mid;
+            else if(arr[mid]<target){
                 st = mid+1;
             }
         }
         
-        if(startindex == -1)
+        if(lastoccurence == -1)
         return 0;
-        else if(startindex == endindex)
+        else if(firstoccurence == lastoccurence)
         return 1;
         else
-        return (endindex - startindex +1);
-        
+        return (lastoccurence - firstoccurence + 1);
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int test_case;
-    cin >> test_case;
-    cin.ignore();
-    while (test_case--) {
-
-        int d;
-        vector<int> arr, brr, crr;
-        string input;
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        getline(cin, input);
-        ss.clear();
-        ss.str(input);
-        while (ss >> number) {
-            crr.push_back(number);
-        }
-        d = crr[0];
-        int n = arr.size();
-        Solution ob;
-        int ans = ob.countFreq(arr, d);
-        cout << ans << endl;
-
-        cout << "~"
-             << "\n";
-    }
-    return 0;
-}
-// } Driver Code Ends
